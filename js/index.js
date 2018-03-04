9bc8b97ed511f5093abf9aee7d7e33e9ac2dca90
Penciler <eraser41@livemail.tw>
2018-03-05 07:13:43 +0800

    add relation between monster class and index.html page

--- js/index.js
+++ js/index.js
@@ -57,11 +57,23 @@ class Hero extends BaseCharacter {
 class Monster extends BaseCharacter {
   constructor(name, hp, ap) {
     super(name, hp, ap);
-    console.log("怪獸遇到 " + this.name + "了！");
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+
+    console.log("遇到怪獸 " + this.name + "了！");
   }
   attack(character) {
     var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
     super.attack(character, Math.floor(damage));
+  }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
   }
 }
 
